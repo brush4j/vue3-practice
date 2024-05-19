5b201d39a35db25a56495a2bca72729e937b32a4
ly <liuyanoutsee@outlook.com>
2024-05-19 20:05:24 +0800

    学到hooks

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,33 +1,20 @@
 <template>
   <div class="person">
-    <h2>姓名：{{name}}</h2>
-    <h2>年龄：{{age}}</h2>
-    <h2>地址：{{address}}</h2>
-    <button @click="changeName">修改名字</button>
-    <button @click="changeAge">修改年龄</button>
-    <button @click="showTel">查看联系方式</button>
+    <h2>当前求和为：{{ sum }}，放大10倍后：{{ bigSum }}</h2>
+    <button @click="add">点我sum+1</button>
+    <hr>
+    <img v-for="(dog,index) in dogList" :src="dog" :key="index">
+    <br>
+    <button @click="getDog">再来一只小狗</button>
   </div>
 </template>
 
 <script lang="ts" setup name="Person">
-  // 数据，原来是写在data中的，此时的name、age、tel都不是响应式的数据
-  let name = '张三'
-  let age = 18
-  let tel = '13888888888'
-  let address = '北京昌平区宏福苑·宏福科技园'
+  import useSum from '@/hooks/useSum'
+  import useDog from '@/hooks/useDog'
 
-  // 方法
-  function changeName() {
-    name = 'zhang-san' //注意：这样修改name，页面是没有变化的
-    console.log(name) //name确实改了，但name不是响应式的
-  }
-  function changeAge() {
-    age += 1 //注意：这样修改age，页面是没有变化的
-    console.log(age) //age确实改了，但age不是响应式的
-  }
-  function showTel() {
-    alert(tel)
-  }
+  const {sum,add,bigSum} = useSum()
+  const {dogList,getDog} = useDog()
 </script>
 
 <style scoped>
@@ -39,5 +26,12 @@
   }
   button {
     margin: 0 5px;
+  }
+  li {
+    font-size: 20px;
+  }
+  img {
+    height: 100px;
+    margin-right: 10px;
   }
 </style>
