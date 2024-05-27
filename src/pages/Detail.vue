4d789cf7469cd21c60998c8afeba63ffa081caf6
ly <liuyanoutsee@outlook.com>
2024-05-27 22:16:12 +0800

    route by pass query

---
+++ src/pages/Detail.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul class="news-list">
+    <li>编号：{{ query.id }}</li>
+    <li>标题：{{ query.title }}</li>
+    <li>内容：{{ query.content }}</li>
+  </ul>
+</template>
+
+<script setup lang="ts" name="About">
+  import {toRefs} from 'vue'
+  import {useRoute} from 'vue-router'
+  let route = useRoute()
+  //必须使用toRefs来解构，结构之后的才会是响应式的
+  let {query} = toRefs(route)
+
+
+</script>
+
+<style scoped>
+  .news-list {
+    list-style: none;
+    padding-left: 20px;
+  }
+
+  .news-list>li {
+    line-height: 30px;
+  }
+</style>
