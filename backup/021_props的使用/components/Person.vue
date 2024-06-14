ebba092f37bc32bf7d8cfadcd0ca5d6cc842292a
lyremote <liuyanoutsee@outlook.com>
2024-06-14 17:22:18 +0800

    update props

--- backup/021_props的使用/components/Person.vue
+++ backup/021_props的使用/components/Person.vue
@@ -18,15 +18,16 @@
   // 接收list + 限制类型
   // defineProps<{list:Persons}>()
 
+  // 接收list，同时将props保存起来
+  /* let x = defineProps(['list'])
+  console.log(x.list) */
+
   //  接收list + 限制类型 + 限制必要性 + 指定默认值
   withDefaults(defineProps<{list?:Persons}>(),{
     list:()=> [{id:'ausydgyu01',name:'康师傅·王麻子·特仑苏',age:19}]
   })
 
 
-  // 接收list，同时将props保存起来
-  /* let x = defineProps(['list'])
-  console.log(x.list) */
 
 </script>
 
